83355fedd95c09b376a70262c5caf02d875940b6
xzw <1272117264@qq.com>
2021-05-26 01:55:57 +0800

    feat: 实现防抖和节流

--- 防抖和节流/index.js
+++ 防抖和节流/index.js
@@ -25,6 +25,30 @@ function throttle(fn, delay = 500) {
   }
 }
 
+// function debounce(fn, delay) {
+//   let timer = null
+//   return function() {
+//     if(timer) {
+//       clearTimeout(timer)
+//     }
+//     timer = setTimeout(() => {
+//       fn.apply(this, arguments)
+//       timer = null
+//     }, delay)
+//   }
+// }
+
+// function throttle(fn, delay) {
+//  let timer = null
+//  return function() {
+//    if(timer) return
+//    timer = setTimeout(() => {
+//     fn.apply(this, arguments)
+//     timer = null
+//    }, delay)
+//  }
+// }
+
 
 const btn1 = document.getElementById('btn1')
 const btn2 = document.getElementById('btn2')
